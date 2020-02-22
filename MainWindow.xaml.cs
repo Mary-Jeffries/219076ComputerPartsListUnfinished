a7c3da656ea32168c883333059e8f6fab8b57b79
Mary-Jeffries <60668218+Mary-Jeffries@users.noreply.github.com>
2020-02-21 22:52:51 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿/*Mary Jeffries
+ * Feb 18- 2020
+ * Displays computer parts
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _219076ComputerParts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParts_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
